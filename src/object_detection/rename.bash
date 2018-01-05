1a7c04c80d313792998e8ff2b24d75d751618ec6
joffman <janosch.hoffmann@hotmail.de>
2018-01-05 18:18:54 +0100

    Removed rename scripts.
    Removed bash scripts that were used to rename file contents
    because they aren't needed anymore.

--- src/object_detection/rename.bash
+++
@@ -1,6 +0,0 @@
-for i in `ls include/object_detection`
-do
-	sed 's/OBJECT_DETECTION_3D/OBJECT_DETECTION/g
-	s/object_detection_3d/object_detection/g' include/object_detection/$i >include/object_detection/new_$i
-	mv include/object_detection/new_$i include/object_detection/$i
-done
--- src/object_detection_3d/rename.bash
+++
@@ -1,6 +0,0 @@
-for i in `ls include/object_detection_3d`
-do
-	sed 's/OBJECT_DETECTION_3D_NODES/OBJECT_DETECTION_3D/g
-	s/object_detection_3d_nodes/object_detection_3d/g' include/object_detection_3d/$i >include/object_detection_3d/new_$i
-	mv include/object_detection_3d/new_$i include/object_detection_3d/$i
-done
