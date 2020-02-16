fa7e84684adf19608d97ee01f8e8a51132c9bb2c
Janosch Hoffmann <janosch.hoffmann@hotmail.de>
2020-02-16 16:06:28 +0100

    Remove whs_navigation package
    No one is interested in this package.
    And I had problems compiling it, because kobuki_msgs wasn't found.

--- src/whs_navigation/scripts/record.bash
+++
@@ -1,5 +0,0 @@
-## Starting the turtlebot/xterm does not work.
-# xterm -e roslaunch whs_navigation turtlebot.launch
-
-cd `rospack find whs_navigation`/bags
-rosbag record -o whs_navigation /scan /tf /tf_static
